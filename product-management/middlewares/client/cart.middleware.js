520214b0f654c62fb5da2f347ff0c445c021ebc6
Yusariii <khailenguyen2004@gmail.com>
2025-09-18 21:30:11 +0700

    Add minicart to header

--- product-management/middlewares/client/cart.middleware.js
+++ product-management/middlewares/client/cart.middleware.js
@@ -1,17 +1,22 @@
 const Cart = require("../../models/cart.model")
 
 module.exports.cartId = async (req, res, next) => {
-    if(!req.cookies.cartId){
+    if (!req.cookies.cartId) {
         const cart = new Cart()
         await cart.save()
-        
+
         const expiresCookie = 7 * 24 * 60 * 60 * 1000
 
         res.cookie("cartId", cart.id, {
             expires: new Date(Date.now() + expiresCookie)
         })
     } else {
+        const cart = await Cart.findOne({
+            _id: req.cookies.cartId
+        })
+        cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0)
 
+        res.locals.miniCart = cart
     }
     next()
 }
