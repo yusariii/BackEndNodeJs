5f18ac36e0fd46691299357d3f2ff46300ce1d2c
Yusariii <khailenguyen2004@gmail.com>
2025-09-20 21:25:57 +0700

    Cart detail

--- product-management/controllers/client/cart.controller.js
+++ product-management/controllers/client/cart.controller.js
@@ -1,11 +1,31 @@
 const Cart = require("../../models/cart.model")
-
+const Product = require("../../models/product.model")
+const productsHelper = require("../../helpers/products")
 // [GET] /cart
 module.exports.index = async (req, res) => {
 
+    const cartId = req.cookies.cartId
+
+    const cart = await Cart.findOne({
+        _id: cartId
+    })
+
+    if (cart.products.length > 0){
+        for(const item of cart.products){
+            const productId = item.product_id
+            const product = await Product.findOne({
+                _id: productId,
+                deleted: false,
+                status: "active"
+            }).select("title thumbnail slug price discountPercentage")
+            productsHelper.calculatePriceNewProduct(product)
+            item.productInfo = product
+        }
+    }
+
     res.render("client/pages/cart/index", {
         pageTitle: "Giỏ hàng",
-
+        cart: cart
     })
 
 }
