0b057d3c480e064b0763ab615028bfaeb01cf788
Yusariii <khailenguyen2004@gmail.com>
2025-09-27 20:06:09 +0700

    Learn SocketIo

---
+++ socketIO/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+
+const app = express();
+const port = 3000;
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+  socket.emit('SERVER_SOCKET_ID', socket.id);
+
+  socket.on("CLIENT_SEND_MSG", (msg) => {
+    console.log("Message from client:", msg);
+
+    // A gửi lên, chỉ A nhận được
+    // socket.emit("SERVER_RETURN_MSG", msg);
+
+    // A gửi lên, cả A, B, C đều nhận được
+    // io.emit("SERVER_RETURN_MSG", msg);
+
+    // A gửi lên, B, C nhận được, trừ A
+    socket.broadcast.emit("SERVER_RETURN_MSG", msg);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
