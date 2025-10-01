5d963d6ae2dba0b8a6867153399c0569e0f18f22
Yusariii <khailenguyen2004@gmail.com>
2025-10-01 16:31:07 +0700

    Done Typing

--- product-management/public/client/js/chat.js
+++ product-management/public/client/js/chat.js
@@ -9,6 +9,7 @@ if (formSendData) {
         if (content) {
             socket.emit("CLIENT_SEND_MESSAGE", content)
             e.target.elements.content.value = ""
+            socket.emit("CLIENT_SEND_TYPING", "hidden")
         }
     })
 }
@@ -18,6 +19,7 @@ if (formSendData) {
 socket.on("SERVER_RETURN_MESSAGE", (data) => {
     const body = document.querySelector(".chat .inner-body")
     const userId = document.querySelector("[my-id]").getAttribute("my-id")
+    const boxListTyping = document.querySelector(".chat .inner-typing-list")
     const div = document.createElement("div")
     div.classList.add((data.user_id == userId ? 'inner-outgoing' : 'inner-incoming'))
     div.innerHTML = `
@@ -30,7 +32,7 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
             ${data.content}
         </div>
     `
-    body.appendChild(div)
+    body.insertBefore(div, boxListTyping)
 
     body.scrollTop = body.scrollHeight
 })
@@ -54,19 +56,39 @@ emojiIcon.onclick = () => {
 // End Toggle Tooltip
 
 
+// Timeout box typing
+var timeoutBoxTyping
+const showTyping = () => {
+    socket.emit("CLIENT_SEND_TYPING", "show")
+
+    clearTimeout(timeoutBoxTyping)
+
+    timeoutBoxTyping = setTimeout(() => {
+        socket.emit("CLIENT_SEND_TYPING", "hidden")
+    }, 5000);
+}
+// End Timeout box typing
+
 // Insert emoji
+
 const emojiPicker = document.querySelector('emoji-picker')
-if(emojiPicker){
+if (emojiPicker) {
     const inputChat = document.querySelector('.chat .inner-form input[name="content"]')
     emojiPicker.addEventListener('emoji-click', event => {
         const icon = event.detail.unicode
         inputChat.value += icon
+
+        const end = inputChat.value.length
+        inputChat.setSelectionRange(end, end)
+        inputChat.focus()
+
+        showTyping()
     })
 }
 // End Insert emoji
 
 // Close tooltip when click outside
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
     if (!emojiIcon.contains(event.target) && !tooltip.contains(event.target)) {
         tooltip.classList.remove('shown');
     }
@@ -75,14 +97,44 @@ document.addEventListener('click', function(event) {
 
 // Typing
 const inputChat = document.querySelector('.chat .inner-form input[name="content"]')
-if(inputChat){
+if (inputChat) {
     inputChat.addEventListener("keyup", () => {
-        socket.emit("CLIENT_SEND_TYPING", "show")
+        showTyping()
     })
 }
 // End Typing
 
 // SERVER RETURN TYPING
-socket.on("SERVER_RETURN_TYPING", (data) => {
-    console.log(data)
-})
+const elementListTyping = document.querySelector(".chat .inner-typing-list")
+if (elementListTyping) {
+    socket.on("SERVER_RETURN_TYPING", (data) => {
+        if (data.type == "show") {
+            const existTyping = elementListTyping.querySelector(`[user-id="${data.user_id}"]`)
+            if (!existTyping) {
+                const body = document.querySelector(".chat .inner-body")
+                const boxTyping = document.createElement("div")
+                boxTyping.setAttribute("user-id", data.user_id)
+                boxTyping.classList.add("box-typing")
+
+                boxTyping.innerHTML = `
+                <div class="inner-info">
+                    <img src="${(data.infoUser.avatar) ? data.infoUser.avatar : '/client/image/avatar_default.jpg'}" alt ="${data.infoUser.fullName}"/>
+                    <span class="inner-name"> ${data.infoUser.fullName} </span>
+                </div>
+                <div class="inner-dots">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            `
+                elementListTyping.appendChild(boxTyping)
+                body.scrollTop = body.scrollHeight
+            }
+        } else {
+            const boxTypingRemove = elementListTyping.querySelector(`[user-id="${data.user_id}"]`)
+            if (boxTypingRemove) {
+                elementListTyping.removeChild(boxTypingRemove)
+            }
+        }
+    })
+}
