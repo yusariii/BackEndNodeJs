8d7cffc02b3db9bac87192c2d7e4a913a5283d6b
Yusariii <khailenguyen2004@gmail.com>
2025-10-27 17:02:45 +0700

    edit api

--- QuanLyCongViec/api/v1/controllers/task.controller.js
+++ QuanLyCongViec/api/v1/controllers/task.controller.js
@@ -143,3 +143,21 @@ module.exports.create = async (req, res) => {
         })
     }
 }
+
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id
+        await Task.updateOne({
+            _id: id
+        }, req.body)
+        res.json({
+            code: 200,
+            message: "Cập nhật công việc thành công"
+        })
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Cập nhật công việc thất bại"
+        })
+    }
+}
--- QuanLyCongViec/api/v1/routes/task.route.js
+++ QuanLyCongViec/api/v1/routes/task.route.js
@@ -12,4 +12,6 @@ router.patch("/change-multi", taskController.changeMulti)
 
 router.post("/create", taskController.create)
 
+router.patch("/edit/:id", taskController.edit)
+
 module.exports = router
